70ff71d6c41c96c41f75a7382d1972c61dbdd546
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2015-12-11 12:12:19 -0300

    [BUG] Renomeando a pasta do projeto o '_' gerou confunsao no django

--- suapwebconf/wsgi.py
+++ suapwebconf/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suap_webconf.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suapwebconf.settings")
 
 application = get_wsgi_application()
